b61fa9b53ae0c3bd46fa078ef013092067c36b91
bjornaah <bjornaah@stud.ntnu.no>
2019-09-06 21:54:48 +0200

    Added plane and other funcionality, can land at runway/crash outside

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform plane;
+
+    [SerializeField]
+    float offsetY, offsetX, distance, smoothTime;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Awake() {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.FindGameObjectWithTag("Plane").transform;
+    }
+    
+    private void FixedUpdate() {
+        Vector3 targetPos = new Vector3(plane.position.x + offsetX, plane.position.y + offsetY, distance);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/planeMovment.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeMovment : MonoBehaviour
+{
+    [SerializeField]
+    float speed, gravity, pitchRate;
+    [SerializeField]
+    float zRotation;
+
+    Rigidbody2D rigidBod2D;
+
+    bool landed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBod2D = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(!landed){
+        if(Input.GetKey(KeyCode.A)){
+            zRotation = pitchRate * Time.deltaTime;
+            rigidBod2D.MoveRotation(rigidBod2D.rotation + zRotation);
+        }
+
+        else if(Input.GetKey(KeyCode.D)){
+            zRotation = -pitchRate * Time.deltaTime;
+            rigidBod2D.MoveRotation(rigidBod2D.rotation + zRotation);
+        }
+        
+        rigidBod2D.MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+        } 
+
+        else{
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, new Quaternion(0,180,-1,0), 1f * Time.deltaTime);
+            rigidBod2D.simulated = false;
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll) {
+        if(coll.gameObject.name == "Ground"){
+            landed = true;
+            Debug.Log("Explode");
+        }
+
+
+
+          if(coll.gameObject.name == "Runway"){
+            landed = true;
+            Debug.Log("Success");
+        }
+    }
+
+}
