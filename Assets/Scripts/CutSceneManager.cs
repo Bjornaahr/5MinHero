f9b687d1e906a97cb0935dc2cb6d535fa7734dde
bjornaah <bjornaah@stud.ntnu.no>
2019-09-07 00:54:26 +0200

    Done with plane landing, started on saving the plane

---
+++ Assets/Scripts/CutSceneManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CutSceneManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject cutScene;
+    [SerializeField]
+    Sprite before, after;
+
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    TextMeshProUGUI flightAttendantText;
+
+    int index;
+
+    [SerializeField]
+    GameObject m_Camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = cutScene.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && index == 0){
+            flightAttendantText.text = "All of our pilots are incapacitated, and we need one of you to pilot the plane";
+            spriteRenderer.sprite = after;
+            index++;
+        }
+
+         else if(Input.GetButtonDown("Fire1") && index == 1){
+            flightAttendantText.text = "Don't worry ATC will tell you what to do";
+            index++;
+        } 
+
+        else if(Input.GetButtonDown("Fire1") && index == 2){
+            m_Camera.transform.position = new Vector3(21,0,-1);
+        }
+
+    }
+}
